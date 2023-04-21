dab6aecc24aed7609ea1771155c2c7b6a14d5f0c
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2023-04-21 01:09:48 -0700

    change func names from policy to strategy

--- src/game/agents.py
+++ src/game/agents.py
@@ -26,14 +26,14 @@ class Sender(Speaker):
 
     def encode(self, state: Meaning) -> Signal:
         """Choose a signal given the state of nature observed, e.g. encode a discrete input as a discrete symbol."""
-        index = self.sample_policy(index=self.referent_to_index(state))
+        index = self.sample_strategy(index=self.referent_to_index(state))
         return self.index_to_expression(index)
 
-    def policy_to_indices(self, policy: dict[str, Any]) -> tuple[int]:
-        """Map a policy to a `(state, signal)` index."""
+    def strategy_to_indices(self, strategy: dict[str, Any]) -> tuple[int]:
+        """Map a strategy to a `(state, signal)` index."""
         return (
-            self.referent_to_index(policy["referent"]),
-            self.expression_to_index(policy["expression"]),
+            self.referent_to_index(strategy["referent"]),
+            self.expression_to_index(strategy["expression"]),
         )
 
 
@@ -51,12 +51,12 @@ class Receiver(Listener):
 
     def decode(self, signal: Signal) -> SignalMeaning:
         """Choose an action given the signal received, e.g. decode a target state given its discrete encoding."""
-        index = self.sample_policy(index=self.expression_to_index(signal))
+        index = self.sample_strategy(index=self.expression_to_index(signal))
         return self.index_to_referent(index)
 
-    def policy_to_indices(self, policy: dict[str, Any]) -> tuple[int]:
-        """Map a policy to a `(signal, state)` index."""
+    def strategy_to_indices(self, strategy: dict[str, Any]) -> tuple[int]:
+        """Map a strategy to a `(signal, state)` index."""
         return (
-            self.expression_to_index(policy["expression"]),
-            self.referent_to_index(policy["referent"]),
+            self.expression_to_index(strategy["expression"]),
+            self.referent_to_index(strategy["referent"]),
         )
