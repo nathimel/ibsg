8d874a4158abc70e9f20dae0f900c1edfce97521
Nathaniel Imel <nimel@hpc3-22-13.local>
2023-04-28 19:47:23 -0700

    hpc multirunning set up

---
+++ scripts/hpc_run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# load conda and activate environment
+module load miniconda3/4.8.5
+conda init bash
+source /opt/apps/miniconda3/4.12.0/etc/profile.d/conda.sh
+conda activate ibsg
+
+echo 'python src/run_simulations.py "$@"'
+python src/run_simulations.py "$@"
+
+echo 'python src/curve.py "$@"'
+python src/curve.py "$@"
+
+echo 'python src/plot.py "$@"'
+python src/plot.py "$@"
