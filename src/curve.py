9f1b48324bce0d1d6dbc0e90373715413d5675b2
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2023-04-29 17:53:26 -0700

    added overwrite flag to stop recomputing curve

--- src/curve.py
+++ src/curve.py
@@ -1,6 +1,6 @@
 """Script to compute the IB curve."""
 import hydra
-import torch
+import os
 from altk.effcomm.information import get_bottleneck, get_rd_curve
 from omegaconf import DictConfig
 from game.game import Game
@@ -16,24 +16,32 @@ def main(config: DictConfig):
     mse_curve_fn = util.get_curve_fn(config, "mse")
 
     g = Game.from_hydra(config)
-    print("computing ib curve...")
 
-    bottleneck = get_bottleneck(
-        prior=g.prior,
-        meaning_dists=g.meaning_dists,
-        maxbeta=g.maxbeta,
-        minbeta=g.minbeta,
-        numbeta=g.numbeta,
-        processes=g.num_processes,
-    )
-    ib_points = list(zip(*bottleneck))
+    if config.game.overwrite_curves or not os.path.isfile(curve_fn):
+        print("computing ib curve...")
 
-    util.save_points_df(curve_fn, util.points_to_df(ib_points, columns=["complexity", "accuracy", "distortion"]))
+        bottleneck = get_bottleneck(
+            prior=g.prior,
+            meaning_dists=g.meaning_dists,
+            maxbeta=g.maxbeta,
+            minbeta=g.minbeta,
+            numbeta=g.numbeta,
+            processes=g.num_processes,
+        )
+        ib_points = list(zip(*bottleneck))
 
-    print("computing mse curve...")
-    mse_points = get_rd_curve(g.prior, g.dist_mat)
+        util.save_points_df(curve_fn, util.points_to_df(ib_points, columns=["complexity", "accuracy", "distortion"]))
+    
+    else:
+        print("data found, skipping ib curve estimation.")
+    
+    if config.game.overwrite_curves or not os.path.isfile(mse_curve_fn):
+        print("computing mse curve...")
+        mse_points = get_rd_curve(g.prior, g.dist_mat)
 
-    util.save_points_df(mse_curve_fn, util.points_to_df(mse_points, columns=["complexity", "mse"]))
+        util.save_points_df(mse_curve_fn, util.points_to_df(mse_points, columns=["complexity", "mse"]))
+    else:
+        print("data found, skipping mse curve estimation")
 
 if __name__ == "__main__":
     main()
